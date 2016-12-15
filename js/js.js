55e1038507b44e7799b9449f71262b9caec0adf7
htmlpluscss <79198889134@ya.ru>
2016-12-15 21:41:47 +0300

    products

--- js/js.js
+++ js/js.js
@@ -58,6 +58,31 @@ http://htmlpluscss.ru
 //		$('#header').toggleClass('header--menu-show');
 //	});
 
+// tabs
+	$.fn.tabs = function(){
+
+		var tab = function(){
+			var t = $(this);
+			var dt = t.children('.tabs__dt');
+			var dd = t.children('.tabs__dd');
+			t.append(dd);
+			dt
+			.wrapAll('<div class="tabs__nav notsel">')
+			.on('click',function(){
+				var t = $(this);
+				t.addClass('tabs__dt--active').siblings('.tabs__dt--active').removeClass('tabs__dt--active');
+				dd.removeClass('tabs__dd--active').eq(dt.index(t)).addClass('tabs__dd--active');
+			})
+			.filter('.tabs__dt--active').length > 0 ?
+				dt.filter('.tabs__dt--active').triggerHandler('click') :
+				dt.first().triggerHandler('click');
+		}
+
+		return this.each(tab);
+
+	};
+
+	$('.tabs').tabs();
 
 
 
