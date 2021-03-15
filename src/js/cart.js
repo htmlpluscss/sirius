13a02d43a81a1f9b8e5359af4e41f17d6ab34687
htmlpluscss <79198889134@ya.ru>
2021-03-15 16:40:35 +0300

    cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,3 +1,32 @@
+// input-pickmeup
+( inputDate => {
+
+	if(!inputDate) {
+
+		return;
+
+	}
+
+	pickmeup.defaults.locales['ru'] = {
+		days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+		daysShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+		daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+		months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+		monthsShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек']
+	};
+
+	pickmeup(inputDate, {
+		min: 'today',
+		hide_on_select : true,
+		title_format: 'B Y',
+		format: 'd.m.Y',
+		locale: 'ru',
+		prev: '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M9 11l4.6-4.6A.99.99 0 1115 7.8l-3.9 3.9 3.9 3.9a.99.99 0 01-1.4 1.4L9 12.41A1 1 0 019 11z"/></svg>',
+		next: '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M15 11a1 1 0 010 1.4L10.4 17A.99.99 0 019 15.6l3.9-3.9L9 7.8a.99.99 0 011.4-1.4L15 11z"/></svg>'
+
+	});
+
+})(document.querySelector('.input-pickmeup'));
 
 // cart
 
