10c875fb461aff2ce9606ed96d5a86992d35b3e2
mixkovrigin <159890280+mixkovrigin@users.noreply.github.com>
2025-05-27 09:35:56 +0300

    каталог

--- src/js/js.js
+++ src/js/js.js
@@ -329,3 +329,66 @@ document.addEventListener("DOMContentLoaded", ()=>{
 	});
 
 });
+
+
+
+( videos => {
+
+	if(videos.length) {
+
+		[...videos].forEach( video => {
+
+			video.addEventListener('click', ()=> {
+
+				if ( video.classList.contains('is-play') === false ) {
+
+					video.classList.add('is-play');
+
+					video.querySelector('video').play();
+					video.querySelector('video').controls = true;
+
+				}
+
+			});
+
+		});
+
+	}
+
+})(document.querySelectorAll('.video'));
+
+( youtube => {
+
+	if(youtube.length) {
+
+		[...youtube].forEach( video => {
+
+			const link = video.querySelector('.youtube__link'),
+				  id = link.getAttribute('data-id');
+
+			link.addEventListener('click', event => {
+
+				event.preventDefault();
+
+				if(video.classList.contains('is-active')){
+
+					return;
+
+				}
+
+				video.classList.add('is-active');
+
+				const iframe = document.createElement('iframe');
+				iframe.setAttribute('controlsList','nodownload');
+				iframe.setAttribute('disablePictureInPicture','true');
+				iframe.setAttribute('allowfullscreen','allowfullscreen');
+				iframe.src = 'https://www.youtube.com/embed/' + id + '?autoplay=1&controls=0';
+				video.append(iframe);
+
+			});
+
+		});
+
+	}
+
+})(document.querySelectorAll('.youtube'));
